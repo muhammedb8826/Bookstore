c0a6bec9104598be122f8b9d609593044ae90311
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-04-01 12:12:25 +0300

    Setup react app and install linters

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import { NavLink } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <NavLink to="/" className="site-title">
+        Bookstore CMS
+      </NavLink>
+      <ul>
+        <li>
+          <NavLink to="/">BOOKS</NavLink>
+        </li>
+        <li>
+          <NavLink to="/calculator">CATEGORIES</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
