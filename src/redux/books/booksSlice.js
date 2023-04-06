dc00d2468c0b2c7a189e02eb010fd45af227eda4
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-04-06 13:55:34 +0300

    Add actions for booksSlice

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -8,6 +8,19 @@ const initialState = {
 const booksSlice = createSlice({
   name: 'book',
   initialState,
+  reducers: {
+    addBook: (state, { payload }) => {
+      state.books.push({
+        id: payload.id,
+        name: payload.name,
+        author: payload.author,
+      });
+    },
+    removeBook: (state, { payload }) => {
+      state.books.filter((book) => book.id !== payload.id);
+    },
+  },
 });
 
+export const { addBook, removeBook } = booksSlice.actions;
 export default booksSlice.reducer;
