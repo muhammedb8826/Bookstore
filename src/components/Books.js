96912054645611aeb611f519779da7d093639bb2
Muhammed Berisso <muhammedb8826@gmail.com>
2023-04-09 00:43:27 +0300

    Added others jsx

--- src/components/Books.js
+++ src/components/Books.js
@@ -40,18 +40,35 @@ export default function Books() {
         <ul>
           {Object.keys(books).length > 0 ? Object.keys(books).map((book) => (
             <li key={book}>
-              <span>{books[book][0].category}</span>
-              <h2>{books[book][0].title}</h2>
-              <p>{books[book][0].author}</p>
-              <div className="buttons">
-                <button type="button">Comments</button>
-                <button
-                  type="button"
-                  onClick={() => { handleClick(book); }}
-                >
-                  Remove
-                </button>
-                <button type="button">Edit</button>
+              <div className="books">
+                <span>{books[book][0].category}</span>
+                <h2>{books[book][0].title}</h2>
+                <p>{books[book][0].author}</p>
+                <div className="buttons">
+                  <button type="button">Comments</button>
+                  <button
+                    type="button"
+                    onClick={() => { handleClick(book); }}
+                  >
+                    Remove
+                  </button>
+                  <button type="button">Edit</button>
+                </div>
+              </div>
+              <div className="progress-and-chapter">
+                <div className="progress">
+                  <div className=".rt-container">
+                    <div className="radialProgressBar progress-60">
+                      <div className="overlay">{}</div>
+                    </div>
+                  </div>
+                  <p className="percent">64%</p>
+                </div>
+                <div className="chapter">
+                  <span>CURRENT CHAPTER</span>
+                  <p>Chapter17</p>
+                  <button type="button">UPDATE PROGRESS</button>
+                </div>
               </div>
             </li>
           )) : <p className="no-book">No books were added</p>}
